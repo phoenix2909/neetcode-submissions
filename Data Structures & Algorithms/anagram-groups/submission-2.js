8d0d11b2db3bd0ea9683a8f21f0031506296b6fa
Sowrabh Sudhir <sowrabh.sneha@gmail.com>
2026-05-17 14:33:08 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        const hMap = new Map()
+        for(let str of strs){
+            const count = new Array(26).fill(0)
+            for(let c of str){
+                count[c.charCodeAt(0) - ('a').charCodeAt(0)] += 1
+            }
+            let key = count.join(',')
+            if(!hMap.has(key)){
+                hMap.set(key, [])
+            }
+            hMap.get(key).push(str)
+        }
+            return Array.from(hMap.values())
+    }
+}

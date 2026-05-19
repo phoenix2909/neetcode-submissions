d962eb5571b725e6dd4af400bdb48fba549cf102
Sowrabh Sudhir <sowrabh.sneha@gmail.com>
2026-05-19 07:57:03 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const hMap = new Map()
+        const freq = new Array(nums.length+1).fill().map(() => [])
+        for(const num of nums){
+            hMap.set(num, (hMap.get(num) || 0) + 1);
+        }
+        for(const [num,count] of hMap.entries()){
+            freq[count].push(num)
+        }
+        let result = []
+        for(let i=freq.length-1; i>=0; i--){
+            result.push(...freq[i])
+            if(result.length >= k){
+                return result.slice(0,k)
+            }
+        }
+    }
+}

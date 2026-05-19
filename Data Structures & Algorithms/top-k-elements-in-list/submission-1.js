110fce2a8c33f9267e5a2226edfc67f073e90705
Sowrabh Sudhir <sowrabh.sneha@gmail.com>
2026-05-19 08:09:30 +0530

    Update: top-k-elements-in-list - submission-1

--- Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.js
@@ -6,18 +6,15 @@ class Solution {
      */
     topKFrequent(nums, k) {
         const hMap = new Map()
-        const freq = new Array(nums.length+1).fill().map(() => [])
+        const freq = new Array(nums.length+1).fill().map(()=>[])
         for(const num of nums){
-            if(!hMap.has(num)){
-                hMap.set(num, 0)
-            }
-            hMap.set(num,hMap.get(num)+1)
+            hMap.set(num,(hMap.get(num) || 0) + 1)
         }
-        for(const [num,count] of hMap.entries()){
+        for(const [num, count] of hMap.entries()){
             freq[count].push(num)
         }
-        let result = []
-        for(let i=freq.length-1; i>=0; i--){
+        const result = []
+        for(let i=freq.length-1;i>=0;i--){
             result.push(...freq[i])
             if(result.length >= k){
                 return result.slice(0,k)
